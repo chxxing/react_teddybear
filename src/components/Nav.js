b2571fdb16886f0e75c7d2054a19d31c03164a13
chxxing <choing26@naver.com>
2022-09-29 14:50:18 +0900

    220929

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,11 @@ import { NavLink } from 'react-router-dom';
 const NAVLINK = [
     { content: "테디베어뮤지엄", link: "/musium" },
     { content: "지점안내", link: "/place" },
-    { content: "공지 & 이벤트", link: "/event" },
+    { content: "공지&이벤트", link: "/event" },
     { content: "기프트샵", link: "/gift" },
     { content: "문의하기", link: "/qna" },
 ]
+
 const Nav = () => {
     return (
         <ul>
--- src/data/common.js
+++ src/data/common.js
@@ -13,4 +13,12 @@ const company_content = {
     copy: "TEDDY BEAR MUSEUM. All Rights Reserved."
 }
 
+const page_content = [
+    { id: 1, title: "테디베어뮤지엄 소개", des: "다양한 테마와 재미있는 볼거리가 가득한 국내 유일의 테디베어 박물관에 오신걸 환영 합니다.", link: "/musium" },
+    { id: 2, title: "지점안내", des: "테디베어뮤지엄은 일년 내내 여러분을 기다리고 있습니다!", link: "/musium" },
+    { id: 3, title: "공지&이벤트", des: "테디베어뮤지엄의 다양한 소식을 만나보세요.", link: "/musium" },
+    { id: 4, title: "기프트샵", des: "정품 핸드메이드 테디베어인형만을 판매하는 프리미업 샵입니다. 14가지 테마로 구성된 기프트샵에서 나만의 반려 인형을 만나보세요!", link: "/musium" },
+    { id: 5, title: "문의하기", des: "상품제작 문의, 상품문의, 프랜차이즈&라이센스, 기타내용을 문의할 수 있는 공간입니다.", link: "/musium" },
+]
+
 export { main_content, company_content };
