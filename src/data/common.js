b5f3a68577fb7b29b9aa93c1c00d524102da8765
chxxing <choing26@naver.com>
2022-10-26 09:49:43 +0900

    221026

--- src/data/common.js
+++ src/data/common.js
@@ -1,9 +1,9 @@
 const main_content = [
-    { id: 1, strong: "국내 유일의 테디베어 박물관", title: "제 주", des: "마티와 함께 떠나는 세계역사탐방", link: "#!" },
-    { id: 2, strong: "국내 유일의 테디베어 박물관", title: "여 수", des: "여수앞바다에 상륙한 하와이 테디베어의 모든 것", link: "#!" },
-    { id: 3, strong: "국내 유일의 테디베어 박물관", title: "군 산", des: "타임요트를 타고 떠나는 시공간 여행", link: "#!" },
-    { id: 4, strong: "국내 유일의 테디베어 박물관", title: "청 두", des: "대륙의 중심, 판다의 고향에서 만나는 테디베어", link: "#!" }
-]
+    { id: 1, strong: "국내 유일의 테디베어 박물관", title: "제 주", des: "마티와 함께 떠나는 세계역사탐방", link: "/place/jeju" },
+    { id: 2, strong: "국내 유일의 테디베어 박물관", title: "여 수", des: "여수앞바다에 상륙한 하와이 테디베어의 모든 것", link: "/place/yeosu" },
+    { id: 3, strong: "국내 유일의 테디베어 박물관", title: "군 산", des: "타임요트를 타고 떠나는 시공간 여행", link: "/place/gunsan" },
+    { id: 4, strong: "국내 유일의 테디베어 박물관", title: "청 두", des: "대륙의 중심, 판다의 고향에서 만나는 테디베어", link: "/place/chengdu" }
+];
 
 const company_content = {
     name: "(주)제이에스앤에프",
@@ -11,7 +11,7 @@ const company_content = {
     ceo: "대표이사 : 김정수",
     address: "본사주소 : 서울시 강남구 언주로 866 7층",
     copy: "TEDDY BEAR MUSEUM. All Rights Reserved."
-}
+};
 
 const page_content = [
     { id: 1, title: "테디베어뮤지엄 소개", des: "다양한 테마와 재미있는 볼거리가 가득한\n국내 유일의 테디베어 박물관에 오신걸 환영 합니다.", link: "/musium" },
@@ -19,6 +19,6 @@ const page_content = [
     { id: 3, title: "공지&이벤트", des: "테디베어뮤지엄의 다양한 소식을 만나보세요.", link: "/event" },
     { id: 4, title: "기프트샵", des: "정품 핸드메이드 테디베어 인형만을 판매하는 프리미업 샵입니다.\n14가지 테마로 구성된 기프트샵에서 나만의 반려 인형을 만나보세요!", link: "/gift" },
     { id: 5, title: "문의하기", des: "상품제작 문의, 상품문의, 프랜차이즈&라이센스,\n기타내용을 문의할 수 있는 공간입니다.", link: "/qna" },
-]
+];
 
 export { main_content, company_content, page_content };
